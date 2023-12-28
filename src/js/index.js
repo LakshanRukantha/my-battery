6205bcb905b2926c2e8b76376361b376246bfedb
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-12-28 20:09:31 +0530

    fix: battery body color not updating properly

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     // API not supported
     document.querySelector(".battery-status").innerText =
-      "Device Not Supported. :(";
+      "Uh-oh! Your device not supported :(";
   }
 
   function updateBatteryInfo(battery) {
@@ -25,16 +25,39 @@ document.addEventListener("DOMContentLoaded", function () {
     let batteryHead = document.querySelector(".battery-head");
     let status = document.querySelector(".battery-status");
 
+    // Display battery status ane level
     if (battery.level <= 0.2) {
-      batteryBody.classList.toggle("bg-red-500");
-      batteryHead.classList.toggle("bg-red-500");
       status.innerText = battery.charging ? "⚡Charging" : "🪫Battery Low";
     } else if (battery.level == 1) {
       status.innerText = "🔋Battery Full";
     } else {
       status.innerText = battery.charging ? "⚡Charging" : "🔌On Battery";
     }
-    status.innerText += ` ${Math.floor(battery.level * 100)}%`;
+
+    // Don't show battery precentage when batter is full
+    if (!(battery.level == 1)) {
+      status.innerText += ` ${Math.floor(battery.level * 100)}%`;
+    }
+
+    // Change battery color when battery level change
+    if (battery.level <= 0.1) {
+      batteryBody.classList.remove("bg-orange-500", "bg-green-500");
+      batteryHead.classList.remove("bg-orange-500", "bg-green-500");
+      batteryBody.classList.add("bg-red-500");
+      batteryHead.classList.add("bg-red-500");
+    } else if (battery.level > 0.1 && battery.level <= 0.2) {
+      batteryBody.classList.remove("bg-red-500", "bg-green-500");
+      batteryHead.classList.remove("bg-red-500", "bg-green-500");
+      batteryBody.classList.add("bg-orange-500");
+      batteryHead.classList.add("bg-orange-500");
+    } else {
+      batteryBody.classList.remove("bg-red-500", "bg-orange-500");
+      batteryHead.classList.remove("bg-red-500", "bg-orange-500");
+      batteryBody.classList.add("bg-green-500");
+      batteryHead.classList.add("bg-green-500");
+    }
+
+    // Change battery level width inside battery body
     batteryBody.setAttribute("style", `width:${battery.level * 200}px`);
   }
 });
